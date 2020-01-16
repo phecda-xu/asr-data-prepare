249d34dac200b341dcf0e9ab24a0fc9138b25eb2
phecda-xu <viaxu910515@163.com>
2020-01-16 11:06:25 +0800

    update

--- THCH30/THCH30.py
+++ THCH30/THCH30.py
@@ -18,30 +18,29 @@ wav_list = [i for i in os.listdir(os.path.join(data_path, 'data')) if i.endswith
 data = pd.DataFrame()
 
 data["wav"] = wav_list
-data["wav_path"] = data.wav.apply(lambda x: "THCH30/data_thchs30/data/{}".format(x))
+data["wav_path"] = data.wav.apply(lambda x: "thch30/THCH30/data_thchs30/data/{}".format(x))
 
 
 def read_text(x):
-    path = os.path.join(BASE_PATH, x)
+    path = os.path.join(os.path.dirname(BASE_PATH), x)
     with open("{}.trn".format(path)) as f:
         a = f.readlines()[0].strip('\n').replace(" ", "")
     return a
 
 
 def read_wav(x):
-    path = os.path.join(BASE_PATH, x)
+    path = os.path.join(os.path.dirname(BASE_PATH), x)
     sig,sr = sf.read(path)
     duration = round(float(len(sig))/float(sr), 3)
     return duration
 
 
-
-data["txt"] = data.wav_path.apply(lambda x:read_text(x))
-data["durations"] = data.wav_path.apply(lambda x:read_wav(x))
+data["txt"] = data.wav_path.apply(lambda x: read_text(x))
+data["durations"] = data.wav_path.apply(lambda x: read_wav(x))
 
 total_duration = round(sum(data.durations)/3600, 4)
 total_wavfile = len(data.wav)
 
 print("total durations is {}h, total wavfiles is {}".format(total_duration, total_wavfile))
 
-data.to_csv(os.path.join(BASE_PATH, "thch30.csv"),index=False)
+data.to_csv(os.path.join(BASE_PATH, "thch30.csv"), index=False)
